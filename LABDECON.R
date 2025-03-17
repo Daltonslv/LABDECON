b1360d1105d0e84b70e5110318c6286439c7aea5
Daltonslv <daltons@furb.br>
2025-03-17 19:50:25 -0300

    Add files via upload

--- LABDECON.R
+++ LABDECON.R
@@ -1,3 +1,26 @@
 # Aula 3
 install.packages("tidyverse")
 
+#install.packages("WDI")
+library(WDI)
+
+# BAIXAR OS DADOS DO PIB ( PRODUTO INTERNO BRUTO )
+# TUDO QUE É PRODUZIDO EM UM PAÍS/ESTADO/MUN.
+# EM UM DETERMINADO PERÍODO
+
+#	GDP (current US$)(NY.GDP.MKTP.CD)
+# GROSS DOMESTIC PRODUCT (GDP) EM DÓLARES NORTE-AMERICAMOS
+# CÓDIGO NY.GDP.MKTP.CD
+
+COD_GDP<- WDISEARCH('GDP')
+# É IMPORTANTE PROCURAR PELO PRÓPRIO
+# SITE DO BANCO MUNDIAL, É MAIS
+# EFICIENTE 
+
+# COM O CÓDIGO, VAMOS BAIXAR OS DADOS
+
+options(scipen = 999)
+
+basepib<- WDI(country = 'all', indicator = 'NY.GDP.MKTP.CD')
+
+basepib2023<- WDI(country = 'all', indicator = 'NY.GDP.MKTP.CD', start = 2023, end = 2023)
